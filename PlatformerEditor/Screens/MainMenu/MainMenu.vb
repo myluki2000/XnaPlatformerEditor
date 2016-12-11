ab5d574415e6d38ffc9dd04cde4c4e5af2013094
myluki2000 <lukas636@live.com>
2016-12-11 01:21:35 +0100

    Initial commit

---
+++ PlatformerEditor/Screens/MainMenu/MainMenu.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.Xna.Framework.Graphics
+
+Namespace MainMenu
+    Public Class MainMenu
+        Public Shared Sub Draw(theSpriteBatch As SpriteBatch)
+            theSpriteBatch.Begin()
+
+            theSpriteBatch.End()
+        End Sub
+    End Class
+End Namespace
